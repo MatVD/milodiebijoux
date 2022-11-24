b949f9d2aa86738594c523d9cdf1157fb6a7afc7
MatVD <vandammemathieu@hotmail.fr>
2022-11-24 06:51:19 +0100

    add contact page and some css on

--- pages/Contact.tsx
+++ pages/Contact.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import styles from "../styles/Contact.module.css"
 
 const Contact = () => {
   return (
-    <div>
+    <div style={{textAlign: 'center'}}>
       <h1>Contact</h1>
+      <h2>Pour toutes questions ou commandes, voici mes coordonées :</h2>
+      <br /><br />
+      <p className={styles.contactName}>Emilie Laviollette</p>
+      <p>Adressse postale</p>
+      <p>Code Postale et ville</p>
+      <br />
+      <p>Ou au 06.12.34.56.78</p>
+      <br />
+      <p>
+        Ou en cliquant ici :{" "}
+        <Link href="mailto: exemple@mail.com">exemple@mail.com</Link>
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
