a84aa96e0a9f6560ab73cd31925ed9b0d9fc7ab8
MatVD <vandammemathieu@hotmail.fr>
2022-11-25 14:42:31 +0100

    add _document.tsx for set head attributes

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -8,15 +8,6 @@ const Layout = ({ children }: any) => {
   return (
 
     <>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@200;400;600&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
       <Header />
       <main>{children}</main>
       <Footer />
